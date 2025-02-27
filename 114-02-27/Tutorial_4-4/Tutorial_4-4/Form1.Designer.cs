f013bad33e9f0b900f413f843831f65757af0d03
cen1007 <A113221006@mail.shu.edu.tw>
2025-02-27 13:02:01 +0800

    114-02-27

---
+++ 114-02-27/Tutorial_4-4/Tutorial_4-4/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Tutorial_4_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            distanceTextBox = new TextBox();
+            gasTextBox = new TextBox();
+            averageLabel = new Label();
+            calculateButton = new Button();
+            exitButton = new Button();
+            logListBox = new ListBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft JhengHei UI", 14F);
+            label1.Location = new Point(146, 79);
+            label1.Name = "label1";
+            label1.Size = new Size(143, 24);
+            label1.TabIndex = 0;
+            label1.Text = "輸入行駛里程數";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft JhengHei UI", 14F);
+            label2.Location = new Point(146, 142);
+            label2.Name = "label2";
+            label2.Size = new Size(143, 24);
+            label2.TabIndex = 1;
+            label2.Text = "消耗油量公升數";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft JhengHei UI", 14F);
+            label3.Location = new Point(146, 211);
+            label3.Name = "label3";
+            label3.Size = new Size(124, 24);
+            label3.TabIndex = 2;
+            label3.Text = "您的平均油耗";
+            // 
+            // distanceTextBox
+            // 
+            distanceTextBox.Font = new Font("Microsoft JhengHei UI", 14F);
+            distanceTextBox.Location = new Point(352, 72);
+            distanceTextBox.Name = "distanceTextBox";
+            distanceTextBox.Size = new Size(149, 31);
+            distanceTextBox.TabIndex = 3;
+            // 
+            // gasTextBox
+            // 
+            gasTextBox.Font = new Font("Microsoft JhengHei UI", 14F);
+            gasTextBox.Location = new Point(352, 142);
+            gasTextBox.Name = "gasTextBox";
+            gasTextBox.Size = new Size(149, 31);
+            gasTextBox.TabIndex = 4;
+            // 
+            // averageLabel
+            // 
+            averageLabel.BorderStyle = BorderStyle.Fixed3D;
+            averageLabel.Font = new Font("Microsoft JhengHei UI", 14F);
+            averageLabel.Location = new Point(352, 211);
+            averageLabel.Name = "averageLabel";
+            averageLabel.Size = new Size(174, 34);
+            averageLabel.TabIndex = 5;
+            averageLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // calculateButton
+            // 
+            calculateButton.Font = new Font("Microsoft JhengHei UI", 14F);
+            calculateButton.Location = new Point(161, 308);
+            calculateButton.Name = "calculateButton";
+            calculateButton.Size = new Size(109, 56);
+            calculateButton.TabIndex = 6;
+            calculateButton.Text = "計算";
+            calculateButton.UseVisualStyleBackColor = true;
+            calculateButton.Click += calculateButton_Click;
+            // 
+            // exitButton
+            // 
+            exitButton.Font = new Font("Microsoft JhengHei UI", 14F);
+            exitButton.Location = new Point(368, 308);
+            exitButton.Name = "exitButton";
+            exitButton.Size = new Size(109, 56);
+            exitButton.TabIndex = 7;
+            exitButton.Text = "離開";
+            exitButton.UseVisualStyleBackColor = true;
+            exitButton.Click += exitButton_Click;
+            // 
+            // logListBox
+            // 
+            logListBox.Font = new Font("Microsoft JhengHei UI", 14F);
+            logListBox.FormattingEnabled = true;
+            logListBox.ItemHeight = 24;
+            logListBox.Location = new Point(604, 53);
+            logListBox.Name = "logListBox";
+            logListBox.Size = new Size(213, 244);
+            logListBox.TabIndex = 8;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Microsoft JhengHei UI", 14F);
+            button1.Location = new Point(643, 319);
+            button1.Name = "button1";
+            button1.Size = new Size(130, 56);
+            button1.TabIndex = 9;
+            button1.Text = "總平均油耗";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(882, 404);
+            Controls.Add(button1);
+            Controls.Add(logListBox);
+            Controls.Add(exitButton);
+            Controls.Add(calculateButton);
+            Controls.Add(averageLabel);
+            Controls.Add(gasTextBox);
+            Controls.Add(distanceTextBox);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox distanceTextBox;
+        private TextBox gasTextBox;
+        private Label averageLabel;
+        private Button calculateButton;
+        private Button exitButton;
+        private ListBox logListBox;
+        private Button button1;
+    }
+}
