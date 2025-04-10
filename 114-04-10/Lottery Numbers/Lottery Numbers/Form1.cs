7e1bd80707b48ea0f8c210c95b475047aa26f273
cen1007 <A113221006@mail.shu.edu.tw>
2025-04-10 13:04:08 +0800

    114-04-10

---
+++ 114-04-10/Lottery Numbers/Lottery Numbers/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lottery_Numbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            const int SIZE = 5; //陣列的大小
+            int[] lotteryNumbers = new int[SIZE];
+            Random rand = new Random();
+
+            for (int i = 0; i < lotteryNumbers.Length; i++)
+            {
+                // 產生1~42的亂數,確認產生的亂數沒有與陣列中元素重複，再放入陣列中
+                int number;
+                do
+                {
+                    number = rand.Next(1, 43);// 產生1~42的亂數
+                } while (lotteryNumbers.Contains(number));
+                lotteryNumbers[i] = number;// 將亂數放入陣列中
+            }
+
+            //將lotteryNumber 陣列中的數字由小到大排序
+            Array.Sort(lotteryNumbers);
+
+            // firstLabel.Text = lotteryNumbers[0].ToString();
+            //secondLabel.Text = lotteryNumbers[1].ToString();
+            // thirdLabel.Text = lotteryNumbers[2].ToString();
+            // fourthLabel.Text = lotteryNumbers[3].ToString();
+            //fifthLabel.Text = lotteryNumbers[4].ToString();
+
+            Label[] showLabels = { firstLabel, secondLabel, thirdLabel, fourthLabel, fifthLabel };
+            for (int i = 0; i < lotteryNumbers.Length; i++)
+            {
+                showLabels[i].Text = lotteryNumbers[i].ToString();
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
---
+++ 114-04-10/Tutorial 7-2/Test Average/Test Average/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Test_Average
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // The Average method accepts an int array argument
+        // and returns the Average of the values in the array.
+        private   Average(   )
+        {
+            int total = 0;
+            foreach (int score in)
+        }
+
+        // The Highest method accepts an int array argument
+        // and returns the highest value in that array.
+        private   Highest(    )
+        {
+           
+        }
+
+        // The Lowest method accepts an int array argument
+        // and returns the lowest value in that array.
+        private  Lowest(   )
+        {
+           
+        }
+
+        private void getScoresButton_Click(object sender, EventArgs e)
+        {
+            const int SIZE = 48;
+            int[] testScores = new int[SIZE];
+            int index = 0;
+            int highestScore = 0;
+            int lowestScore = 0;
+            double averageScore = 0.0;
+            StreamReader inputFile;
+            try
+            {
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    inputFile = File.OpenText(openFile.FileName);
+                    while (!inputFile.EndOfStream && index <SIZE)
+                    {
+                        testScores[index] = Convert.ToInt32(inputFile.ReadLine());
+                        index++;
+                    }
+                    inputFile.Close();
+
+                    averageScore = Average(testScores);
+                    highestScore = Highest(testScores);
+                    lowestScore = Lowest(testScores);
+
+                    averageScoreLabel.Text = averageScore.ToString("n2");
+                    highestScoreLabel.Text = highestScore.ToString();
+                    lowScoreLabel.Text = lowestScore.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
